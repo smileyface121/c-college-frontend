60191528908df2f231839c3e9a334ae2b58b4ec3
smileyface121 <syed.read09@gmail.com>
2025-05-17 23:54:01 -0400

    fix: safelist dynamic bg classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  safelist: [
+    'bg-blue-200',
+    'bg-red-200',
+    'bg-green-200',
+    'hover:bg-blue-100',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
